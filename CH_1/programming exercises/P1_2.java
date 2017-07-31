bd835430d9e80f86751d581af67ba3cb18599bdb
Abdelrahman_Attia <abdelrahmanatia94@outlook.com>
2017-07-31 22:00:16 +0300

    p1_2 modified

--- CH_1/programming exercises/P1_2.java
+++ CH_1/programming exercises/P1_2.java
@@ -5,6 +5,15 @@ public class P1_2 {
 
 	public static void main(String[] args) 
 	{
-		System.out.println(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10);
+		int sum = 0;
+		for(int i = 1; i <= 10; i++)
+		{
+			sum = sum + i;
+		}
+		
+		System.out.println(sum);
+		
+		//or u can do this
+		//System.out.println(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10);
 	}
 }
