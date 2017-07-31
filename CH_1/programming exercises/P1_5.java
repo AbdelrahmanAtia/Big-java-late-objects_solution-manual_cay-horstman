1fcf113be89c7e4f8e4236e828ed07a0aa8e778b
Abdelrahman_Attia <abdelrahmanatia94@outlook.com>
2017-07-31 22:29:51 +0300

    P1_5 added

---
+++ CH_1/programming exercises/P1_5.java
@@ -0,0 +1,17 @@
+/*
+ *(P1.5) Write a program that displays your name inside a box on the screen, like this:
+   +----+
+   |Dave|
+   +----+
+  Do your best to approximate lines with characters such as | - +.
+*/
+
+public class P1_5 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("+----+");
+		System.out.println("|Dave|");
+		System.out.println("+----+");
+	}
+}
