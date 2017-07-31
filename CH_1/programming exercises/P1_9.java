145487f35f05ccfe811895d0c504daba8efdde66
Abdelrahman_Attia <abdelrahmanatia94@outlook.com>
2017-07-31 23:57:42 +0300

    P1_9 added

---
+++ CH_1/programming exercises/P1_9.java
@@ -0,0 +1,26 @@
+/*
+ *(P1.9) Write a program that prints a house that looks exactly like the following:
+ 
+	   +  
+	+     +
+	+ 	  +
+	+-----+
+	| .-. |
+	| | | |
+	+-+-+-+
+ */
+public class P1_9 {
+
+	public static void main(String[] args) {
+
+		System.out.println("   +  ");
+		System.out.println("+     +");
+		System.out.println("+     +");
+		System.out.println("+-----+");
+		System.out.println("| .-. |");
+		System.out.println("| | | |");
+		System.out.println("+-+-+-+");
+
+	}
+
+}
