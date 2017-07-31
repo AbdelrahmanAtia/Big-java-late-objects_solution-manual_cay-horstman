7b3f4b9aca3b7487cb7b5de51e1283925c82aa3c
Abdelrahman_Attia <abdelrahmanatia94@outlook.com>
2017-07-31 21:45:58 +0300

    P1_1 added

---
+++ CH_1/programming exercises/P1_1.java
@@ -0,0 +1,11 @@
+/*
+ * (P1.1) Write a program that prints a greeting of your choice, perhaps in a language other than English.
+ */
+
+public class P1_1
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Bonjour");
+	}
+}
