bdf1d222068f24bddc59ce701913e76bb05f5161
AbdelrahmanAtia <abdelrahmanatia94@outlook.com>
2017-08-04 11:37:57 +0300

    P1_14 added

---
+++ CH_1/programming exercises/P1_14.java
@@ -0,0 +1,20 @@
+/*
+(P1.14) Type in and run the following program:
+	import javax.swing.JOptionPane;
+	public class DialogViewer
+	{
+		public static void main(String[] args)
+		{
+			JOptionPane.showMessageDialog(null, "Hello, World!");
+		}
+	}
+	Then modify the program to show the message “Hello, your name!”.
+*/
+import javax.swing.JOptionPane;
+
+public class P1_14 {
+
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "Hello, Abdo!");
+	}
+}
