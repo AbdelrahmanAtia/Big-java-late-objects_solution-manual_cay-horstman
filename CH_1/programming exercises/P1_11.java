ad70f0f7bb2db83eb2070adf8d7a4a9b7994b5d4
AbdelrahmanAtia <abdelrahmanatia94@outlook.com>
2017-08-04 11:36:02 +0300

    P1_11 added

---
+++ CH_1/programming exercises/P1_11.java
@@ -0,0 +1,14 @@
+/*
+	(P1.11) Write a program that prints three items, such as the names of your three best friends
+	 or favorite movies, on three separate lines. 
+*/
+
+public class P1_11 {
+
+	public static void main(String[] args) {
+
+		System.out.println("Ahmad");
+		System.out.println("Ali");
+		System.out.println("Hassan");
+	}
+}
